a57449bebcedf6ecb2b8cbb0417260e12f143e61
Oliver Baylis <oli.baylis@gmail.com>
2018-07-18 21:19:34 +0100

    Add extra properties as per ef core docs

--- HelpdeskApp/Models/DataItem.cs
+++ HelpdeskApp/Models/DataItem.cs
@@ -9,7 +9,9 @@ namespace HelpdeskApp.Models
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public int ContactId { get; set; }
         public Contact Contact { get; set; }
+        public int AssignmentGroupId { get; set; }
         public AssignmentGroup AssignmentGroup { get; set; }
     }
 }
